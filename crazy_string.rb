518672e22f42e41a3ea92b4d4f07450929eb84ef
randihi <randihigashi@gmail.com>
2018-06-06 05:00:48 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,4 +2,9 @@
 
 def crazy_strings(a,b)
 
++def crazy_strings(first_string, second_string)
++  a = a.reverse.upcase
++  second_string = second_string.swapcase.gsub("s", "z")
++  return "#{first_string} #{second_string}"
+
 end
